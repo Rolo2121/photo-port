f7c1e7159a56001e8339fb2146243a5da0b98982
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-08-02 15:25:34 -0500

    added submit functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 import './App.css';
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
 			/>
 			<main>
 				<div>
+					<ContactForm />
 					<Gallery currentCategory={currentCategory} />
 					<About />
 				</div>
